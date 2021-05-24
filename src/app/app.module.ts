4cbf3fcc040a4753d7c1873a074ab04e7c745a94
jshchen <42899831+jshchen@users.noreply.github.com>
2021-05-23 23:41:50 -0400

    Remove fakeeditor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { SidebarItemComponent } from './components/sidebar-item/sidebar-item.component';
 import { EditorComponent } from './components/editor/editor.component';
-import { FakeEditorComponent } from './components/fake-editor/fake-editor.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
@@ -18,8 +17,6 @@ import { HttpClientModule } from '@angular/common/http';
     SidebarComponent,
     SidebarItemComponent,
     EditorComponent,
-    FakeEditorComponent,
-
   ],
   imports: [
     BrowserModule,
